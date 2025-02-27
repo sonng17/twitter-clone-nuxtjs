d66a792d8bfa9d896677244e9bd9559c0501b29d
Nguyễn Đình Sơn <ndson1703@gmail.com>
2025-02-27 09:56:33 +0700

    app overview and starting leftsidebar 2

--- app.vue
+++ app.vue
@@ -6,14 +6,13 @@
           class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5"
         >
           <!-- Left sidebar -->
-          <div
-            class="md:block xs:col-span-1 xl:col-span-2"
-          >
+          <div class="hidden md:block xs:col-span-1 xl:col-span-2">
             <div class="sticky top-0">
               <SidebarLeft />
             </div>
           </div>
           <!-- Main content -->
+          
           <!-- Right sidebar -->
         </div>
       </div>
